7c4f2d5908b9dff3f9a6f033e2cf20e6d738ac1b
olaf majoor <323666@student.mboutrecht.nl>
2019-05-15 15:32:15 +0200

    dit is een commit via de vsc zonder terminal

--- class_text.php
+++ class_text.php
@@ -3,6 +3,7 @@ class Tekst {
 
     var $wattekst = "dit is een tekst met een alternative fact erin.";
     var $nieuwetekst = "dit is nog een tekst blalalabw";
+    var $tekst2 = "dit is een commit via vsc";
 }
 
 ?>
